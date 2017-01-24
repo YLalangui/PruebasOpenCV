185035905cca7fc97b5511b459c0563f8defeaff
Yerhard <yera.lalangui@gmail.com>
2017-01-24 01:46:36 +0100

    Add files via upload

---
+++ Capitulo4/Capitulo4/read-image-fun.py
@@ -0,0 +1,52 @@
+import cv2
+import os
+import numpy as np
+import errno
+import sys
+
+
+def read_images(path, sz=None):
+    c = 0
+    X, y = [], []
+    for dirname, dirnames, filenames in os.walk(path):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                try:
+                    if (filename == ".directory"):
+                        continue
+                    filepath = os.path.join(subject_path, filename)
+                    im = cv2.imread(os.path.join(subject_path, filename), cv2.IMREAD_GRAYSCALE)
+                    # resize to given size (if given)
+                    if (sz is not None):
+                        im = cv2.resize(im, (200, 200))
+                    X.append(np.asarray(im, dtype=np.uint8))
+                    y.append(c)
+                except IOError:
+                    print ("I/O error({0}): {1}".format(errno))
+                except:
+                    print("Unexpected error:", sys.exc_info()[0])
+                    raise
+            c = c+1
+
+    return [X,y]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
